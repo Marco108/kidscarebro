4261bc01a49a0ad008367c8cab782fdf351ea1d8
phimaly <phammanhphi3011@gmail.com>
2019-11-12 15:21:10 +0700

    sua new file

--- lib/screen/newfile.dart
+++ lib/screen/newfile.dart
@@ -4,7 +4,9 @@ class NewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("sajhasjcsjhacjhsac"),
+      ),
     );
   }
 }
